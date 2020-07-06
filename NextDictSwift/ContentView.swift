d32cc5c5d599e74cd1b9b6c8f50fcaa1a6ba94f8
Hao Nguyen <hao.ict56@gmail.com>
2020-07-07 07:22:57 +0900

    Add assets and init code
    https://www.hackingwithswift.com/quick-start/swiftui/composing-views-to-create-a-list-row

--- NextDictSwift/ContentView.swift
+++ NextDictSwift/ContentView.swift
@@ -9,34 +9,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selection = 0
- 
-    var body: some View {
-        TabView(selection: $selection){
-            Text("First View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("first")
-                        Text("First")
-                    }
-                }
-                .tag(0)
-            Text("Second View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("second")
-                        Text("Second")
-                    }
-                }
-                .tag(1)
+  @State private var selection = 0
+  
+  let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+  
+  var body: some View {
+    TabView(selection: $selection){
+      NavigationView {
+        List {
+          ForEach(menu) { section in
+            Text(section.name)
+            ForEach(section.items) { item in
+              Text(item.name)
+            }
+          }
         }
+        .navigationBarTitle("Menu")
+        .listStyle(GroupedListStyle())
+      }
+      .tabItem {
+        VStack {
+          Image("first")
+          Text("First")
+        }
+      }
+      .tag(0)
+      Text("Second View")
+        .font(.title)
+        .tabItem {
+          VStack {
+            Image("second")
+            Text("Second")
+          }
+      }
+      .tag(1)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
